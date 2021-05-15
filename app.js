6833a5476a45303a946a700b66c24423f24eeea7
steveplayshorn <steveplayshorn@gmail.com>
2021-05-14 23:33:45 -0400

    Cleaned up app.js.

--- app.js
+++ app.js
@@ -17,26 +17,34 @@ const main = document.querySelector('main')
 const footer = document.querySelector('footer')
 const instructionContainer = document.querySelector('.instruction-container')
 const readyBtn = document.querySelector('#ready')
-const milli100 = 500;
+const time = 500
+
+let pattern = [btn1, btn2, btn3, btn4]
+let randomPattern = pattern.sort(() => Math.random() - 0.5)
+let patternClicks = randomPattern.length
+let playerPattern = []
+let playerClicks = 0
+let level = 1
+let isPlayingPattern = false
+let cardArr = []
 
 //Game ready
 readyBtn.addEventListener('click', ready)
 
+function ready() {
+    instructionContainer.remove()
+    btns.forEach(btn => btn.style.display='block')
+}
+
 //Start the game
 startBtn.addEventListener('click', start)
 
-//computer pattern choices
-let pattern = [btn1, btn2, btn3, btn4]
-
-let randomPattern = pattern.sort(() => Math.random() - 0.5)
-
-let patternClicks = randomPattern.length
-
-//Tracks players selections and btn clicks
-let playerPattern = []
-let playerClicks = 0
-let level = 1
-let isPlayingPattern = false;
+function start() {
+    setTimeout(() => {
+        runPattern(randomPattern)
+    }, 1000)
+    startBtn.remove()
+}
 
 //Shows the computer pattern that the player must copy
 function runPattern(order) {
@@ -44,62 +52,62 @@ function runPattern(order) {
     btnFlash(order[0])
     setTimeout(() => {
         btnFlash(order[1])
-    }, milli100)
+    }, time)
     setTimeout(() => {
         btnFlash(order[2])
-    }, milli100*2)
+    }, time*2)
     setTimeout(() => {
         btnFlash(order[3])
         isPlayingPattern = false
-    }, milli100*3)
+    }, time*3)
     setTimeout(() => {
         if(level === 2) {
             btnFlash(order[4])
             isPlayingPattern = false
         }  
-    }, milli100*4)
+    }, time*4)
     setTimeout(() => {
         if(level === 3) {
             setTimeout(() => {
                 btnFlash(order[4])    
-            }, milli100);
+            }, time)
             setTimeout(() => {
                 btnFlash(order[5])
                 isPlayingPattern = false
-            }, milli100 * 2);
+            }, time * 2)
         }  
-    }, milli100*3.2)
+    }, time*3.2)
     setTimeout(() => {
         if(level === 4) {
             setTimeout(() => {
                 btnFlash(order[4])    
-            }, milli100);
+            }, time)
             setTimeout(() => {
                 btnFlash(order[5])
-            }, milli100 * 2);
+            }, time * 2)
             setTimeout(() => {
                 btnFlash(order[6])
                 isPlayingPattern = false
-            }, milli100 * 3);  
+            }, time * 3)
         }  
-    }, milli100*3.2)
+    }, time*3.2)
     setTimeout(() => {
         if(level === 5) {
             setTimeout(() => {
                 btnFlash(order[4])    
-            }, milli100);
+            }, time)
             setTimeout(() => {
                 btnFlash(order[5])
-            }, milli100 * 2);
+            }, time * 2)
             setTimeout(() => {
                 btnFlash(order[6])
-            }, milli100 * 3);
+            }, time * 3)
             setTimeout(() => {
                 btnFlash(order[6])
                 isPlayingPattern = false
-            }, milli100 * 4);   
+            }, time * 4)  
         }  
-    }, milli100*3.2)
+    }, time*3.2)
 }
 
 //button flashes when computer selects it
@@ -107,7 +115,7 @@ function btnFlash(btn) {
     btn.classList.toggle('selected')
     setTimeout(() => {
         btn.classList.toggle('selected')
-    }, milli100/4)
+    }, time/4)
 }
 
 //Adds playerClick to all game buttons
@@ -117,22 +125,18 @@ btns.forEach(btn => btn.addEventListener('click', playerClick))
 function playerClick(e) {
     if(!isPlayingPattern) {
         e.target.classList.add('userSelect')
-    setTimeout(() => {
-        e.target.classList.remove('userSelect')
-    }, 150)
-    playerClicks += 1
-    playerPattern.push(e.target)
-    if(playerClicks === patternClicks) {
-        result()
-    }
+        setTimeout(() => {
+            e.target.classList.remove('userSelect')
+        }, 150)
+        playerClicks += 1
+        playerPattern.push(e.target)
+        if(playerClicks === patternClicks) {
+            result()
+        }
     }
 }
 
 //Result compares playerPattern input to pattern
-//if arrays are same, win
-    //next level
-//if arrays are different, lose
-    //game over
 function result() {
     let result1 = (arr1, arr2) => {
         for(let i = 0; i < arr1.length; i++) {
@@ -147,9 +151,9 @@ function result() {
         nextLevel()
     } else {
         winEl.innerHTML = `
-        <h2>You lose</h2>
-        <button class='start' id='playAgain'><i class="far fa-play-circle"></i></button>
-        <p>Play again?</p>
+            <h2>You lose</h2>
+            <button class='start' id='playAgain'><i class="far fa-play-circle"></i></button>
+            <p>Play again?</p>
         `
         const playAgain = document.querySelector('#playAgain')
         playAgain.addEventListener('click', function() {
@@ -159,28 +163,28 @@ function result() {
 }
 
 //Called by result() if winning condition met
-//Resets DOM elements and starts next level
 function nextLevel() {
     if(level !== 5) {
         setTimeout(() => {
             showCardChoices()
-        if (cardArr.length === level) {
-            setTimeout(() => {
-                levelChange()
-                hideCardChoices()
-                fetching()
-            }, 1000); /* Timer until next level starts, after card picked*/
-        } else {
-            setTimeout(() => {
-                nextLevel()
-            }, 1000) /* Timer to check if card has been picked */
-        }
-        }, 2000); /* Timer until cardChoices shown */
+            if (cardArr.length === level) {
+                setTimeout(() => {
+                    levelChange()
+                    hideCardChoices()
+                    fetching()
+                }, time) /* Timer until next level starts, after card picked */
+            } else {
+                setTimeout(() => {
+                    nextLevel()
+                }, time * 2) /* Timer to check if card has been picked */
+            }
+        }, 1000) /* Timer until cardChoices shown */
     } else {
         youWin()
     }
 }
 
+//Resets DOM and makes a new pattern
 function levelChange() {
     pattern.push(pattern[Math.floor(Math.random())*pattern.length])
     let newPattern = pattern.sort(() => Math.random() - 0.5)
@@ -197,58 +201,45 @@ function levelChange() {
     }, 3000)
 }
 
+//Shows the win screen and ends the game
 function youWin() {
     if(level === 5) {
-        levelEl.remove()
-        winEl.remove()
-        btnsContainer.remove()
         showCardChoices()
         setTimeout(() => {
             cardArrFunc()
             showDeck()
             hideCardChoices()
+            cleanUp()
             const gameOver = document.createElement('div')
-        gameOver.classList.add('winScreen')
-        gameOver.innerHTML = `
-        <h1>You Win!</h1>
-        <h2>Here's your deck!</h2>
-        <p>Play again?</p>
-        <button class='start' id='playAgain'><i class="far fa-play-circle"></i></button>
-        `
-        header.classList.add('bg-overlay')
-        main.classList.add('bg-overlay')
-        footer.classList.add('bg-overlay')
-        headerTitle.style.opacity = '0'
-        headerSub.style.opacity = '0'
-        // header
-        // main
-        // footer
-        // add bg-overlay class
-        main.append(gameOver)
-        const playAgain = document.querySelector('#playAgain')
-        playAgain.addEventListener('click', function() {
-            location.reload()
-        })
-        }, 5000);
-        
-        
+            gameOver.classList.add('winScreen')
+            gameOver.innerHTML = `
+                <h1>You Win!</h1>
+                <h2>Here's your deck!</h2>
+                <p>Play again?</p>
+                <button class='start' id='playAgain'><i class="far fa-play-circle"></i></button>
+            `
+            main.append(gameOver)
+            const playAgain = document.querySelector('#playAgain')
+            playAgain.addEventListener('click', function() {
+                location.reload()
+            })
+        }, 5000)  
     }
 }
 
-//Ready function
-function ready() {
-    instructionContainer.remove()
-    btns.forEach(btn => btn.style.display='block')
+//Removes DOM elements for win screen
+function cleanUp() {
+    levelEl.remove()
+    winEl.remove()
+    btnsContainer.remove()
+    header.classList.add('bg-overlay')
+    main.classList.add('bg-overlay')
+    footer.classList.add('bg-overlay')
+    headerTitle.style.opacity = '0'
+    headerSub.style.opacity = '0'
 }
 
-//starts the game when start button clicked
-function start() {
-    setTimeout(() => {
-        runPattern(randomPattern)
-    }, 1000)
-    startBtn.remove()
-}
-//143-176 commented out for phone data
+//Fetch API data and creates 3 random cards
 async function fetching() {
     fetch('https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/sets/classic', {
     headers: {
@@ -266,6 +257,8 @@ async function fetching() {
 }
 
 fetching()
+
+//Creates a random card from the API data
 function createCard(data) {
     const randNum = Math.floor(Math.random() * 388)
     if(data[randNum].img) {
@@ -282,78 +275,53 @@ function createCard(data) {
     }
 }
 
-let cardArr = []
-
+//Adds the selected card to the players deck
 function pickCard(e) {
     const card = e.target
     cardArr.push(card.src)
     card.classList.add('added')
-    console.log(card.src)
 }
 
-
+//Displays the card choices
 function showCardChoices() {
     cardPicker.style.display = 'flex'
 }
 
+//Hides the card choices
 function hideCardChoices() {
     cardPicker.style.display = 'none'
     cardPicker.innerHTML = ''
 }
 
+//Shows the players deck
 function showDeck() {
-    deckContainer.style.display = 'flex'
-    
+    deckContainer.style.display = 'flex' 
 }
 
-function cardArrFunc() {
-    cardArr.forEach((card) => {
-        console.log(deckContainer.firstElementChild)
-        if(!deckContainer.firstElementChild){
-        deckContainer.innerHTML += `
-        <img src="${card}" alt="">
-        ` 
-    } else if(deckContainer.lastElementChild.childNodes) {
-        //if deckContainer.childNodes contains card 
-        // console.log(deckContainer.childNodes[1])
-        // deckContainer.childNodes.forEach((node) => {
-        //     if(node.contains(card)) {
-        //         console.log('node test')
-        //     }
-        // })
-            console.log(deckContainer.lastElementChild.src)
-            if(deckContainer.lastElementChild.src !== card) {
-                console.log(card)
-                // cardArr.forEach(card => {
-                //     const cardImg = document.createElement('img')
-                //     cardImg.src = card
-                //     console.log(cardImg)
-                //     deckContainer.appendChild(cardImg)
-                // })
-                deckContainer.innerHTML += `
-                <img src="${card}" alt="">
-                `  
-            }
-    }
-})
-}
-
+//Hides the players deck
 function hideDeck() {
     deckContainer.style.display = 'none'
 }
 
+//Populates the DOM with the players deck card images
+function cardArrFunc() {
+    cardArr.forEach((card) => {
+        if(!deckContainer.firstElementChild){
+        deckContainer.innerHTML += `
+            <img src="${card}" alt="">
+        ` 
+        } else if(deckContainer.lastElementChild.childNodes) {
+            if(deckContainer.lastElementChild.src !== card) {
+                deckContainer.innerHTML += `
+                    <img src="${card}" alt="">
+                `  
+            }
+        }
+    })
+}
+
 //Cheat
-const title = document.querySelector('h1')
-title.addEventListener('click', function() {
+headerTitle.addEventListener('click', function() {
     playerPattern = pattern
     result()
 })
-
-//font on github pages
-//line 236 stuff
-//refactor
-    // What would you do differently?
-    // What are you most proud of?
-    // What would you do next?
-    // How did you plan your project?
-    // What did you learn?
